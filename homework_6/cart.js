b4ced9684196b24b739b42d7ac821060066dd348
Corey Emery <cemery@andrew.cmu.edu>
2018-10-28 22:55:19 -0400

    Cart page built beside the item content

---
+++ homework_6/cart.js
@@ -0,0 +1,40 @@
+function createCartList(items) {
+
+};
+
+$(document).ready(function(){
+    var cart = JSON.parse(localStorage.getItem("cart"))
+
+    if (cart === null) {
+        cart = []
+        localStorage.setItem("cart", JSON.stringify(cart));
+    }
+    $("#nav-cart").text("Cart (" + cart.length + ")");
+
+
+    $(document).on("click", ".shop-glazing-option", function() {
+        $(this).parent().children().removeAttr("id");
+        $(this).attr("id", "glaze-selected")
+    });
+
+    $(document).on("click", ".shop-quantity-option", function() {
+        $(this).parent().children().removeAttr("id");
+        $(this).attr("id", "quantity-selected")
+    });
+
+    $(document).on("click", "#add-to-cart", function() {
+        localStorage.removeItem("cart");
+        var selection = {
+            quantity: $("#quantity-selected").text(),
+            glaze: $("#glaze-selected").text(),
+            price: $("#price-option").text(),
+            name: $("#product-title").text(),
+            img: $("#modal-image").attr("src")
+        };
+        cart.push(selection)
+
+        $("#nav-cart").text("Cart (" + cart.length + ")");
+
+        localStorage.setItem("cart", JSON.stringify(cart));
+    });
+});
